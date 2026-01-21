3f2d95f024342d9f33f0e34896ed2cdd54b557c1
Toy93 <yqs_ahut@163.com>
2026-01-21 22:28:38 +0800

    aa

--- CPU/rtl/filelist.f
+++
@@ -1,7 +0,0 @@
-$DESIGN_ROOT/$PROJ_DIR/rtl/alu.v
-$DESIGN_ROOT/$PROJ_DIR/rtl/decoder.v
-$DESIGN_ROOT/$PROJ_DIR/rtl/pc.v
-$DESIGN_ROOT/$PROJ_DIR/rtl/pico_mips.v
-$DESIGN_ROOT/$PROJ_DIR/rtl/program_memory.v
-$DESIGN_ROOT/$PROJ_DIR/rtl/regs.v
-$DESIGN_ROOT/$PROJ_DIR/rtl/sp_ram_256x24.v
